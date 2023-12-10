546ca6a43cc22bfff4ff89283643f90c39470615
Mikolaj <mikolaj.zachas@icloud.com>
2023-12-10 16:56:15 +0100

    Update

--- blog-app/resources/views/admin/header.blade.php
+++ blog-app/resources/views/admin/header.blade.php
@@ -2,11 +2,11 @@
   <nav class="navbar navbar-expand-lg">
     <div class="search-panel">
       <div class="search-inner d-flex align-items-center justify-content-center">
-        <div class="close-btn">Close <i class="fa fa-close"></i></div>
+        <div class="close-btn"> <i class="fa fa-close"></i></div>
         <form id="searchForm" action="#">
           <div class="form-group">
             <input type="search" name="search" placeholder="What are you searching for...">
-            <button type="submit" class="submit">Search</button>
+            <button type="submit" class="submit"></button>
           </div>
         </form>
       </div>
